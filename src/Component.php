3ed2d3907e8d477be8cebc453100a8d23093cce4
Vesko Kenashkov <vesko@azonmedia.com>
2020-08-31 12:53:23 +0300

    - suppressed eslint for the generated file

--- src/Component.php
+++ src/Component.php
@@ -8,7 +8,7 @@ class Component
 {
     protected const COMPONENT_NAME = "Vue router package";
     protected const COMPONENT_URL = 'https://vue-router.packages.guzaba.org/';
-    protected const COMPONENT_NAMESPACE = 'Azonmedia\\VueRouter';
+    protected const COMPONENT_NAMESPACE = __NAMESPACE__;
     protected const COMPONENT_VERSION = '0.0.1';
     protected const VENDOR_NAME = 'Azonmedia';
     protected const VENDOR_URL = 'https://azonmedia.com';
